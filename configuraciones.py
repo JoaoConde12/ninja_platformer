e15f99b2613a339fcd426ec1e13c6070657ab66f
Joao Conde <132232545+JoaoConde12@users.noreply.github.com>
2023-05-01 03:08:10 -0500

    configuraciones iniciales del juego, mini mapa

---
+++ configuraciones.py
@@ -0,0 +1,18 @@
+mapa = [
+'                                  ',
+'                                  ',
+'xx         xxx                xx  ',
+'xx                                ',
+'xxxx               xx            x',
+'xxxx             xx               ',
+'xx         x   xxxx      xx   xx  ',
+'           x   xxxx      xx   xxx ',
+'        xxxx   xxxxxx    xx   xxxx',
+'xxxxxxxxxxxx   xxxxxx    xx   xxxx',
+]
+
+tamaño_bloque = 64
+ventana_ancho = 1200
+ventana_alto = len(mapa) * tamaño_bloque
+
+print(len(mapa))
