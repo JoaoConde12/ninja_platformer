e975cee1dc1791d7cc648ad44ab7f31706825926
Joao Conde <132232545+JoaoConde12@users.noreply.github.com>
2023-05-06 16:10:59 -0500

    Añadir gravedad y salto al jugador

--- codigo/jugador.py
+++ codigo/jugador.py
@@ -6,8 +6,12 @@ class Jugador(pygame.sprite.Sprite):
         self.image = pygame.Surface((32, 64))
         self.image.fill((255, 0, 0))
         self.rect = self.image.get_rect(topleft = posicion)
+
+        #Movimiento del jugador
         self.direccion = pygame.math.Vector2(0, 0)
         self.velocidad = 7
+        self.gravedad = 0.8
+        self.velocidad_salto = -16
 
     def obtener_teclas(self):
         teclas = pygame.key.get_pressed()
@@ -19,6 +23,17 @@ class Jugador(pygame.sprite.Sprite):
         else:
             self.direccion.x = 0
 
+        if teclas[pygame.K_SPACE]:
+            self.saltar()
+
+    def aplicar_gravedad(self):
+        self.direccion.y += self.gravedad
+        self.rect.y += self.direccion.y
+
+    def saltar(self):
+        self.direccion.y = self.velocidad_salto
+
     def update(self):
         self.obtener_teclas()
         self.rect.x += self.direccion.x * self.velocidad
+        self.aplicar_gravedad()
