ef7d68f55efa3d92f6851df421cc0e6a90c3b380
Joao Conde <132232545+JoaoConde12@users.noreply.github.com>
2023-05-06 15:51:03 -0500

    cambio de nombre de variables del mapa

---
+++ codigo/configuraciones.py
@@ -0,0 +1,15 @@
+mapa = [
+'                            ',
+'                            ',
+' XX    XXX            XX    ',
+' XX P                       ',
+' XXXX         XX         XX ',
+' XXXX       XX              ',
+' XX    X  XXXX    XX  XX    ',
+'       X  XXXX    XX  XXX   ',
+'    XXXX  XXXXXX  XX  XXXX  ',
+'XXXXXXXX  XXXXXX  XX  XXXX  ']
+
+tamaño_bloque = 64
+ventana_ancho = 1200
+ventana_alto = len(mapa) * tamaño_bloque
