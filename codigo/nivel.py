9df7f715905420e951c06ca28e8585219645900d
Joao Conde <132232545+JoaoConde12@users.noreply.github.com>
2023-06-05 14:32:20 -0500

    Añadidos algunos bloques del nivel

--- codigo/nivel.py
+++ codigo/nivel.py
@@ -1,31 +1,55 @@
 #Importación de librerías y módulos
 import pygame
-from bloques import Bloque
+from data_niveles import nivel1
 from configuraciones import tamaño_bloque, ventana_ancho
-from jugador import Jugador
+from importador import importar_csv_layout, importar_graficos_terreno
+from bloques import Bloque
+from vegetacion import Arbol1, Arbol2, Tronco
+from agua import Agua, Olas
+
 
 class Nivel:
-    def __init__(self, mapa_beta, surface):
-        self.mostrar_surface = surface
-        self.configurar_nivel(mapa_beta)
-        self.avanzar_mapa = 0
-        self.x_actual = 0
+    def __init__(self, nivel, surface):
+
+        #Configuración general
+        self.ventana_surface = surface
+        self.cambio_mundo = 0
+
+        #Jugador
 
 
-    def configurar_nivel(self, layout):
-        self.bloques = pygame.sprite.Group()
-        self.jugador = pygame.sprite.GroupSingle()
+        #Configuración de terreno
+        terreno_layout = importar_csv_layout(nivel1["terreno"])
+        self.terreno_sprites = self.crear_grupos_bloques(terreno_layout, "terreno")
 
-        for fila_indice, fila in enumerate(layout):
-            for columna_indice, columna in enumerate(fila):
-                x = columna_indice * tamaño_bloque
-                y = fila_indice * tamaño_bloque
-                if columna == 'X':    
-                    bloque = Bloque((x, y), tamaño_bloque)
-                    self.bloques.add(bloque)
-                if columna == 'P':
-                    jugador = Jugador((x, y))
-                    self.jugador.add(jugador)
+
+        #Arboles y troncos
+        arbol1_layout = importar_csv_layout(nivel1["arbol1"])
+        self.arbol1_sprites = self.crear_grupos_bloques(arbol1_layout, "arbol1")
+
+        arbol2_layout = importar_csv_layout(nivel1["arbol2"])
+        self.arbol2_sprites = self.crear_grupos_bloques(arbol2_layout, "arbol2")
+
+        tronco_layout = importar_csv_layout(nivel1["tronco"])
+        self.tronco_sprites = self.crear_grupos_bloques(tronco_layout, "tronco")
+
+
+        #Enemigo
+        enemigo_layout = importar_csv_layout(nivel1["enemigo"])
+        self.enemigo_sprites = self.crear_grupos_bloques(enemigo_layout, "enemigo")
+
+        #Choque de enemigo
+        choque_layout = importar_csv_layout(nivel1["choque"])
+        self.choque_sprites = self.crear_grupos_bloques(choque_layout, "choque")
+
+
+        #Agua y olas
+        agua_layout = importar_csv_layout(nivel1["agua"])
+        self.agua_sprites = self.crear_grupos_bloques(agua_layout, "agua")
+
+        olas_layout = importar_csv_layout(nivel1["ola"])
+        self.olas_sprites = self.crear_grupos_bloques(olas_layout, "ola")
+
 
 
     def mover_camara_x(self):
@@ -48,7 +72,7 @@ class Nivel:
         jugador = self.jugador.sprite
         jugador.rect.x += jugador.direccion.x * jugador.velocidad
 
-        for sprite in self.bloques.sprites():
+        for sprite in self.terreno_sprites.sprites():
             if sprite.rect.colliderect(jugador.rect):
                 if jugador.direccion.x < 0:
                     jugador.izquierda = True
@@ -64,13 +88,12 @@ class Nivel:
         if jugador.derecha and (jugador.rect.right > self.x_actual or jugador.direccion.x <= 0):
             jugador.derecha = False
         
-        
 
     def colision_vertical(self):
-        jugador = self.jugador.sprite
+        jugador = self.jugador.sprite   
         jugador.aplicar_gravedad()
-
-        for sprite in self.bloques.sprites():
+    
+        for sprite in self.terreno_sprites.sprites():
             if sprite.rect.colliderect(jugador.rect):
                 if jugador.direccion.y > 0:
                     jugador.rect.bottom = sprite.rect.top
@@ -85,16 +108,5 @@ class Nivel:
                 jugador.en_suelo = False
             if jugador.en_techo and jugador.direccion.y > 0:
                 jugador.en_techo = False
-
-
-    def ejecutar(self):
-        self.bloques.update(self.avanzar_mapa)
-        self.bloques.draw(self.mostrar_surface)
-        self.mover_camara_x()
-
-        self.jugador.update()        
-        self.colision_horizontal()
-        self.colision_vertical()
-        self.jugador.draw(self.mostrar_surface)
 
         
