34b071fef047c965ba3e0f544318ea1918b1554f
Joao Conde <132232545+JoaoConde12@users.noreply.github.com>
2023-06-05 13:46:27 -0500

    Nuevas configuraciones de medidas de ventana

--- codigo/configuraciones.py
+++ codigo/configuraciones.py
@@ -1,15 +1,3 @@
-mapa = [
-'                            ',
-'                            ',
-' XX    XXX            XX    ',
-' XX P                       ',
-' XXXX         XX         XX ',
-' XXXX       XX              ',
-' XX    X  XXXX    XX  XX    ',
-'       X  XXXX    XX  XXX   ',
-'    XXXX  XXXXXX  XX  XXXX  ',
-'XXXXXXXX  XXXXXX  XX  XXXX  ']
-
 tamaño_bloque = 64
-ventana_ancho = 1200
-ventana_alto = len(mapa) * tamaño_bloque
+ventana_ancho = 1284
+ventana_alto = 704
