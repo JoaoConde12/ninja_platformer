7d4177abaaadfeb0b8f4376efc85cf10e99451c9
Joao Conde <132232545+JoaoConde12@users.noreply.github.com>
2023-06-05 13:57:53 -0500

    Csv del primer nivel

---
+++ codigo/data_niveles.py
@@ -0,0 +1,21 @@
+nivel1 = {
+    "agua": "../niveles/nivel1_csv/nivel1_agua.csv",
+    "arbol1": "../niveles/nivel1_csv/nivel1_arbol1.csv",
+    "arbol2": "../niveles/nivel1_csv/nivel1_arbol2.csv",
+    "arbusto1": "../niveles/nivel1_csv/nivel1_arbusto1.csv",
+    "arbusto2": "../niveles/nivel1_csv/nivel1_arbusto2.csv",
+    "arbusto3": "../niveles/nivel1_csv/nivel1_arbusto3.csv",
+    "arbusto4": "../niveles/nivel1_csv/nivel1_arbusto4.csv",
+    "cajas": "../niveles/nivel1_csv/nivel1_cajas.csv",
+    "cartel1": "../niveles/nivel1_csv/nivel1_cartel1.csv",
+    "cartel2": "../niveles/nivel1_csv/nivel1_cartel2.csv",
+    "enemigo": "../niveles/nivel1_csv/nivel1_enemigo.csv",
+    "monedas": "../niveles/nivel1_csv/nivel1_monedas.csv",
+    "mushroom_dec": "../niveles/nivel1_csv/nivel1_mushroom_dec.csv",
+    "mushroom_salto": "../niveles/nivel1_csv/nivel1_mushroom_salto.csv",
+    "olas": "../niveles/nivel1_csv/nivel1_olas.csv",
+    "personaje": "../niveles/nivel1_csv/nivel1_personaje.csv",
+    "rocas": "../niveles/nivel1_csv/nivel1_rocas.csv",
+    "terreno": "../niveles/nivel1_csv/nivel1_terreno.csv",
+    "troncos": "../niveles/nivel1_csv/nivel1_troncos.csv"
+}
