09ae207aa0175ca3e7f7219a3ffba1d3c510fcdd
Joao Conde <132232545+JoaoConde12@users.noreply.github.com>
2023-06-05 14:57:05 -0500

    Corregida la importación de gráficos

--- codigo/vegetacion.py
+++ codigo/vegetacion.py
@@ -5,20 +5,20 @@ from bloques import Bloque_estatico
 
 class Arbol1(Bloque_estatico):
     def __init__(self, tamaño, x, y):
-        super().__init__(tamaño, x, y, pygame.image.load("../graficos/arboles/arbol1/0.png").convert_alpha())
+        super().__init__(tamaño, x, y, pygame.image.load("../graficos/arboles/arbol1/arbol1.png").convert_alpha())
         desplazar_y = y + tamaño
         self.rect = self.image.get_rect(bottomleft = (x, desplazar_y))
 
 
 class Arbol2(Bloque_estatico):
     def __init__(self, tamaño, x, y):
-        super().__init__(tamaño, x, y, pygame.image.load("../graficos/arboles/arbol2/2.png").convert_alpha())
+        super().__init__(tamaño, x, y, pygame.image.load("../graficos/arboles/arbol2/arbol2.png").convert_alpha())
         desplazar_y = y + tamaño
         self.rect = self.image.get_rect(bottomleft = (x, desplazar_y))
 
         
 class Tronco(Bloque_estatico):
     def __init__(self, tamaño, x, y):
-        super().__init__(tamaño, x, y, pygame.image.load("../graficos/arboles/tronco/1.png").convert_alpha())
+        super().__init__(tamaño, x, y, pygame.image.load("../graficos/arboles/tronco/tronco.png").convert_alpha())
         desplazar_y = y + tamaño
         self.rect = self.image.get_rect(bottomleft = (x, desplazar_y))
