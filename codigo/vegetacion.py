736f9477b272fa915cab1f778cb134588a77241c
Joao Conde <132232545+JoaoConde12@users.noreply.github.com>
2023-06-05 19:03:50 -0500

    Añadidos los arbustos

--- codigo/vegetacion.py
+++ codigo/vegetacion.py
@@ -22,3 +22,31 @@ class Tronco(Bloque_estatico):
         super().__init__(tamaño, x, y, pygame.image.load("../graficos/arboles/tronco/tronco.png").convert_alpha())
         desplazar_y = y + tamaño
         self.rect = self.image.get_rect(bottomleft = (x, desplazar_y))
+
+
+class Arbusto1(Bloque_estatico):
+    def __init__(self, tamaño, x, y):
+        super().__init__(tamaño, x, y, pygame.image.load("../graficos/arbustos/arbusto1/arbusto1.png").convert_alpha())
+        desplazar_y = y + tamaño
+        self.rect = self.image.get_rect(bottomleft = (x, desplazar_y))
+
+
+class Arbusto2(Bloque_estatico):
+    def __init__(self, tamaño, x, y):
+        super().__init__(tamaño, x, y, pygame.image.load("../graficos/arbustos/arbusto2/arbusto2.png").convert_alpha())
+        desplazar_y = y + tamaño
+        self.rect = self.image.get_rect(bottomleft = (x, desplazar_y))
+
+
+class Arbusto3(Bloque_estatico):
+    def __init__(self, tamaño, x, y):
+        super().__init__(tamaño, x, y, pygame.image.load("../graficos/arbustos/arbusto3/arbusto3.png").convert_alpha())
+        desplazar_y = y + tamaño
+        self.rect = self.image.get_rect(bottomleft = (x, desplazar_y))
+
+
+class Arbusto4(Bloque_estatico):
+    def __init__(self, tamaño, x, y):
+        super().__init__(tamaño, x, y, pygame.image.load("../graficos/arbustos/arbusto4/arbusto4.png").convert_alpha())
+        desplazar_y = y + tamaño
+        self.rect = self.image.get_rect(bottomleft = (x, desplazar_y))
