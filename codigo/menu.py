866f62f3150c9754cded75085f3c0a6560c2229a
Joao Conde <132232545+JoaoConde12@users.noreply.github.com>
2023-06-08 03:12:46 -0500

    Clase ícono

--- codigo/menu.py
+++ codigo/menu.py
@@ -6,13 +6,17 @@ class Nodo(pygame.sprite.Sprite):
     def __init__(self, posicion, estado, velociad_icono):
         super().__init__()
         self.image = pygame.Surface((100, 80))
+
         if estado == "disponible":
             self.image.fill("blue")
         else:
             self.image.fill("grey")
+            
         self.rect = self.image.get_rect(center = posicion)
 
         self.detectar_colision = pygame.Rect(self.rect.centerx - (velociad_icono / 2),
                                              self.rect.centery - (velociad_icono / 2),
                                              velociad_icono,
                                              velociad_icono)
+        
+
